1bff3399c1d7e203b78ddf7789b558cc7721b05f
Jared Robertson <robertsonjareds@gmail.com>
2026-01-22 15:23:05 -0600

    Revert to simple version

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-importScripts('config.js'); // Loads self.MYSM_CONFIG
+importScripts('config.js'); 
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
